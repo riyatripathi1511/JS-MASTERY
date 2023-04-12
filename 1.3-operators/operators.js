301632f812c914d11de2a14448990ae84995bb5d
Riya Tripathi <riyatripathi1511@gmail.com>
2023-04-12 20:35:07 +0530

    operands and operator in js

---
+++ 1.3-operators/operators.js
@@ -0,0 +1,37 @@
+// alert("hello")
+
+//Operator and operand
+var num1 = 22;
+console.log(num1)
+var num1 = 22;
+num1 = -num1;//- is the unary operator and num1 is operand,bcz the - is working on only 1 operand its called unary operator
+console.log(num1);
+ var num2 = 25;
+ var result = 2+num2;//+ is the binary operator and num1 is operand,bcz the + is working on only 2 operand its called binary operator
+// '*','/',-
+console.log(result);//shortcut = shift+alt+down-arrow
+
+var x = 5;
+var y = 2;
+//arithmetic operation
+console.log("Result of addition is="+(x+y));
+console.log("Result of subtraction="+(x-y));
+console.log("Result of multiplication="+(x*y));
+console.log("Result of division="+(x/y));
+console.log("Result of exponent="+(x**y));//25
+console.log("Result of x++="+(x++));//5
+console.log("Result of ++x="+(++x));//5
+
+y=y+1;//y=y+1
+y += 1;//y=y+1;
+y -= 1; //y= y-1
+y *= y //y*y;
+
+console.log("Hello"+" - "+"World");
+var firstName ="John";
+var lastName ="Doe";
+
+console.log(y); 
+console.log("Your firstname is "+firstName+" "+"and lastName is "+lastName+" "+" hence your fullname is "+firstName+" "+lastName);
+
+console.log(`Your firstname is ${firstName} and lastName is lastName hence your fullname is ${firstName} ${lastName}`);
