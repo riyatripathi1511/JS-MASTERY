fb40b3975bebbf00f13d4ba47953ddfe84bfb63d
Riya Tripathi <riyatripathi1511@gmail.com>
2023-04-22 01:52:59 +0530

    Calculator

--- project-Calculator/project-calculator.js
+++ project-Calculator/project-calculator.js
@@ -1,5 +1,24 @@
 const calculate = () => {
-    let opr = document.querySelector("#operation");
-    let num1 = document.querySelector("#num1");
-    let num2 = document.querySelector("#num2");
+    let opr = document.querySelector("#operation").value;
+    let num1 = document.querySelector("#num1").value;
+    let num2 = document.querySelector("#num2").value;
+      if( num1  == '' || num2 ==='' ){
+        alert(" One or more fields are empty");
+        return; 
+      }
+      num1 = parseFloat(num1);
+      num2 = parseFloat(num2);
+      let result = 0.0;
+
+      if(opr === 'add'){
+        result = num1+num2;
+      }else if(opr ==='sub'){
+        result= num1 - num2;
+      }else if(opr ==='mul'){
+        result= num1 * num2;
+      }else if(opr ==='div'){
+        result= num1 / num2;
+      }
+    //debugger;
+    document.querySelector("#result").innerHTML=`Result is : ${Math.round(result)}`;
 }
