b15cd0814f5fd69c21243630e5ec86abe72794b7
Riya Tripathi <riyatripathi1511@gmail.com>
2023-04-22 14:15:21 +0530

    Project

---
+++ 1.25-Fetch data/main.js
@@ -0,0 +1,17 @@
+const getPosts = ()=>{
+  let fetchPosts = fetch("https://jsonplaceholder.typicode.com/posts");
+   let allPostTitle = '';
+  fetchPosts.then((response) => {
+    response.json().then((jsonData) => {
+       //console.log(jsonData);
+       for(i=0;i<jsonData.length;i++){
+        let post =  jsonData[i];
+       // debugger;
+      // console.log(post.title);   
+       allPostTitle = allPostTitle+`<strong>Post title:</strong>${post.title} <br> <strong> Post body :</strong>${post.body}<br><br> <hr>`;
+       }
+       document.querySelector("#allPosts").innerHTML = allPostTitle;
+    });
+  
+});
+}
